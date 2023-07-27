610288efddea63bf5a59e34c4f299593f9c284b7
LuisRamon <luisr2f@gmail.com>
2023-07-27 08:32:50 -0400

    fix header

--- src/_global/axios.tsx
+++ src/_global/axios.tsx
@@ -8,8 +8,6 @@ axiosInstance.interceptors.request.use((config: any) => {
   config.headers.Authorization = `Bearer ${personalToken}`
   config.headers['Access-Control-Allow-Origin'] = '*'
   config.headers['Content-Type'] = 'text/plain'
-  config.headers['Access-Control-Allow-Headers'] =
-    'Origin, X-Requested-With, Content-Type, Accept'
 
   return config
 })
